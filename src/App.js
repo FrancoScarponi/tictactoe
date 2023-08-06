aa75d50ba22f59b0122212b225ca1c38e97b019c
Franco Scarponi <francoscarponi02@gmail.com>
2023-08-06 16:41:16 -0300

    finaliza al ganar

--- src/App.js
+++ src/App.js
@@ -5,23 +5,54 @@ import { CuadradoTurno } from './components/CuadradoTurno';
 
 
 
+
+
 function App() {
   const turnos = {
     X: "x",
     O: "o"
   }
 
+  const combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+
   const [tabla, setTabla] = useState(Array(9).fill(null))
   const [turno, setTurno] = useState(turnos.X)
+  const [gano, setGano] = useState(null)
 
-  const actualizarTabla = (indice)=>{
-    if (tabla[indice] === null){
-      let nuevaTabla = [... tabla]
-      nuevaTabla[indice] = turno
-      setTabla(nuevaTabla)
-      setTurno(turno===turnos.X ? turnos.O : turnos.X)
+  const checkGana = (boardToCheck) => {
+    for (const combo of combos) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
     }
-    
+    return null
+  }
+
+  const actualizarTabla = (indice) => {
+    let nuevaTabla = [...tabla]
+    if (gano || tabla[indice]) return
+
+    nuevaTabla[indice] = turno
+    setTabla(nuevaTabla)
+    setTurno(turno === turnos.X ? turnos.O : turnos.X)
+
+
+    const newGanador = checkGana(nuevaTabla)
+    setGano(newGanador)
+
+
+
   }
 
 
@@ -30,19 +61,19 @@ function App() {
       <h1>Tic tac toe</h1>
       <section className='tabla'>
         {
-          tabla.map((a, indice) =>{
+          tabla.map((a, indice) => {
             return (
-              
+
               <Cuadrado key={indice} indice={indice} tabla={tabla} actualizarTabla={actualizarTabla}></Cuadrado>
-              
+
             )
           })
         }
       </section>
 
       <section className='turnos'>
-        <CuadradoTurno turnos={turnos.X} seleccionado={turno===turnos.X}></CuadradoTurno>
-        <CuadradoTurno turnos={turnos.O} seleccionado={turno===turnos.O}></CuadradoTurno>
+        <CuadradoTurno turnos={turnos.X} seleccionado={turno === turnos.X}></CuadradoTurno>
+        <CuadradoTurno turnos={turnos.O} seleccionado={turno === turnos.O}></CuadradoTurno>
       </section>
     </main>
   );
